2f08d48718c020e3486186ebbee731f9a0b22772
Sadiq <sadiqwali@gmail.com>
2022-05-26 14:40:52 -0700

    Commits changes on project1 and project3

--- project1/main.cs
+++ project1/main.cs
@@ -3,7 +3,7 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Project1- Change5");
+            System.Console.WriteLine("Project1- Change6");
         }
     }
 }
