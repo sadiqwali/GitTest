e46029ca5771f4ac89972a370464e987cfde6796
sadiqwali <67432014+sadiqwali@users.noreply.github.com>
2022-05-26 14:03:38 -0700

    Create new file
    Create new file

---
+++ Project2/HelloWorld.cs
@@ -0,0 +1,9 @@
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Project2-Change1");
+        }
+    }
+}
